43bd0349496aa9e9ae597a65a779d5503b8812c9
Gnana Prakash M <gnanaprakash2918@gmail.com>
2024-05-11 21:32:08 +0530

    Update Task Class

--- src/modules/Task.js
+++ src/modules/Task.js
@@ -1,9 +1,21 @@
 export default class Task {
-  constructor(name, dueDate = 'No Date') {
+  constructor(
+    name,
+    dueDate = 'No Date',
+    description = '',
+    note = '',
+    completionStatus = false,
+    priority
+  ) {
     this.name = name;
     this.dueDate = dueDate;
+    this.isCompleted = completionStatus;
+    this.priority = priority;
+    this.note = note;
+    this.description = description;
   }
 
+  // Name
   setName(name) {
     this.name = name;
   }
@@ -12,6 +24,7 @@ export default class Task {
     return this.name;
   }
 
+  // Due Date
   setDate(date) {
     this.dueDate = date;
   }
@@ -20,6 +33,44 @@ export default class Task {
     return this.dueDate;
   }
 
+  // Completion Status
+  getCompletionStatus() {
+    return this.isCompleted;
+  }
+
+  setCompletionStatus(status) {
+    this.isCompleted = status;
+  }
+
+  // Priority
+  // 0 -> Low, 1 -> Normal, 2 -> High
+  getPriority() {
+    return this.priority;
+  }
+
+  setPriority(priority) {
+    this.priority = priority;
+  }
+
+  // Note
+  getNote() {
+    return this.note;
+  }
+
+  setNote(note) {
+    this.note = note;
+  }
+
+  // Description
+  getDescription() {
+    return this.description;
+  }
+
+  setDescription(description) {
+    this.description = description;
+  }
+
+  // Get Formatted Date
   getDateFormatted() {
     const [day, month, year] = this.dueDate.split('/');
     return `${month}/${day}/${year}`;
