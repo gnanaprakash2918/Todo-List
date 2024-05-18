2c05e13790a9ddc95ffa1382fcae210be3f7e5fe
Gnana Prakash M <gnanaprakash2918@gmail.com>
2024-05-19 00:30:40 +0530

    Align icons in left-panel

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import './style.css';
+
 import TodoListLogo from './Images/TodoList.png';
+import inboxIcon from './Images/inbox-icon.png';
+import thisWeekIcon from './Images/this-week-icon.png';
+import todayIcon from './Images/today-icon.png';
+import addIcon from './Images/add-icon.png';
+import importantIcon from './Images/important-icon.png';
+
 import UI from './modules/UI';
 
 console.log('working fine');
