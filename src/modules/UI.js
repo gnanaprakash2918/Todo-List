11d565e2aa772e0d5a985418e3936a3ca8864317
Gnana Prakash M <gnanaprakash2918@gmail.com>
2024-05-19 15:21:12 +0530

    Add Task Modal dynamically to overlay innerhtml

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -6,16 +6,64 @@ import Project from './Project';
 // Event Listeners
 
 const modalOverlay = document.querySelector('.overlay');
-const addTaskModal = document.querySelector('.add-task-modal');
 const addTaskBtn = document.querySelector('.add-task');
-const closeTaskModalBtn = document.querySelector('.task-close-btn');
 
 addTaskBtn.addEventListener('click', () => {
   modalOverlay.classList.add('bg');
-  addTaskModal.classList.add('visible');
-});
+  modalOverlay.innerHTML = `
+                    <div class="add-task-modal modal">
+                    <!-- <label for="task-name">Task Name</label> -->
+                    <input
+                      type="text"
+                      name="task-name"
+                      id="task-name"
+                      placeholder="Enter Task Name"
+                      required
+                    />
+                    <!-- <label for="task-desc">Description</label> -->
+                    <input
+                      type="text"
+                      name="task-desc"
+                      id="task-desc"
+                      placeholder="Task Description"
+                    />
+                    <!-- <label for="task-notes">Notes</label> -->
+                    <textarea
+                      name="task-notes"
+                      id="task-notes"
+                      placeholder="Task Notes"
+                    ></textarea>
+                    <!-- <label for="due-date">Due Date</label> -->
+                    <input
+                      type="text"
+                      id="due-date"
+                      name="due-date"
+                      placeholder="Due Date"
+                      onfocus="this.type = 'date'"
+                      onblur="this.type = 'text'"
+                    />
+                    <!-- <label for="priority">Task Priority</label> -->
+                    <select name="priority" id="priority">
+                      <option value="" disabled selected>Select Priority</option>
+                      <option value="low">Low</option>
+                      <option value="normal">Normal</option>
+                      <option value="high">High</option>
+                    </select>
+                    <button class="task-add-btn btn">Add</button>
+                    <button class="task-close-btn btn">Close</button>
+                  </div>
+  `;
 
-closeTaskModalBtn.addEventListener('click', () => {
-  modalOverlay.classList.remove('bg');
-  addTaskModal.classList.remove('visible');
+  const addTaskModal = document.querySelector('.add-task-modal');
+  const closeTaskModalBtn = document.querySelector('.task-close-btn');
+
+  const closeModal = () => {
+    modalOverlay.classList.remove('bg');
+    modalOverlay.innerHTML = '';
+  };
+
+  closeTaskModalBtn.addEventListener('click', () => {
+    closeModal();
+    closeTaskModalBtn.removeEventListener('click', closeModal);
+  });
 });
